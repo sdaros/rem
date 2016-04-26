540e6ab8250c1c740b567085999eb565865bf915
Stefano Da Ros <sdaros@cip.li>
2016-04-26 08:20:54 +0200

    Move add_reminder to handlers package
    We did this so that we can properly include the buildMetadata in the
    version number when building the package.

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/sdaros/rem/handlers"
+)
+
+const (
+	// buildMetadata is replaced when package is built using -ldflags -X
+	// ex: go build -ldflags "-X main.buildMetadata=+`git rev-parse --short HEAD`"
+	buildMetadata = ""
+	version       = "0.1.0"
+	name          = "rem"
 )
 
 func main() {
 	port := ":42888"
 	mux := http.NewServeMux()
 	mux.Handle("/", AddReminder())
-	log.Printf("Serving on %v", port)
+	log.Printf("Serving %v (version: %v) on %v", name, Version(), port)
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 
 }
+
+func Version() string { return version + buildMetadata }
