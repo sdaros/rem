4b7d2717a8aa7990b357adbfb85cd13adb4fe59d
Stefano Da Ros <sdaros@cip.li>
2016-04-27 09:59:14 +0200

    Added registrar to manage environment variables

--- rem.go
+++ rem.go
@@ -3,8 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/sdaros/rem/handlers"
+	"sync"
 )
 
 var (
@@ -13,14 +12,39 @@ var (
 	name          = "rem"
 )
 
+type Registrar struct {
+	values map[string]interface{}
+	sync.Mutex
+}
+
 func main() {
 	port := ":42888"
 	mux := http.NewServeMux()
-	mux.Handle("/", handlers.AddReminder())
+	mux.Handle("/", AddReminder())
 	log.Printf("Serving %v (version: %v) on %v", name, Version(), port)
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 
+}
+
+func (r *Registrar) Register(k string, v interface{}) {
+	if r == nil {
+		return
+	}
+
+	r.Lock()
+	defer r.Unlock()
+	r.values[k] = v
+}
+
+func (r *Registrar) Lookup(k string) interface{} {
+	if r == nil {
+		return nil
+	}
+
+	r.Lock()
+	defer r.Unlock()
+	return r.values[k]
 }
 
 func Version() string { return version + buildMetadata }
